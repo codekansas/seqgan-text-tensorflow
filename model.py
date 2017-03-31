a1273b412d38c51306f755bfcc07d7648e938322
Benjamin Bolte <bkbolte18@gmail.com>
2017-03-31 10:32:39 -0400

    added supervisor

--- model.py
+++ model.py
@@ -1,9 +1,12 @@
 """Utils for the TensorFlow model."""
 
+from collections import OrderedDict
 import warnings
 
 import numpy as np
 import tensorflow as tf
+
+from keras.optimizers import Adam
 
 import utils
 
@@ -26,7 +29,32 @@ def get_scope_variables(scope):
         list of variables.
     """
 
-    return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=scope)
+    return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope)
+
+
+def multinomial_3d(x):
+    """Samples from a multinomial distribution from 3D Tensor.
+    Args:
+        x: Tensor with shape (batch_size, timesteps, classes)
+    Returns:
+        Tensor with shape (batch_size, timesteps), sampled from `classes`.
+    """
+    a, b = tf.shape(x)[0], tf.shape(x)[1]
+    x = tf.reshape(x, (a * b, -1))
+    m = tf.multinomial(x, 1)
+    return tf.reshape(m, (a, b))
+
+
+def multinomial_2d(x):
+    """Samples from a multinomial distribution from 2D Tensor.
+    Args:
+        x: Tensor with shape (batch_size, classes)
+    Returns:
+        Tensor with shape (batch_size), sampled from `classes`.
+    """
+    a = tf.shape(x)[0]
+    m = tf.multinomial(x, 1)
+    return tf.reshape(m, (a,))
 
 
 class SeqGAN(object):
@@ -34,13 +62,17 @@ class SeqGAN(object):
     Args:
         sess: an active TF session to use for the model.
         num_classes: int, number of output classes (i.e. characters).
+        learn_phase: int (default: None), the phase (i.e. one generator step
+            per cycle, the rest discriminator steps). If None, the
+            discriminator and generator are updated simultaneously.
+        log_every: int (default: 50), how often to save tensor summaries.
         only_cpu: bool (default: False), if set, only build weights on CPU
             (useful for deploying a trained model to a production server).
         logdir: str, where to save each model epoch.
         num_latent: int, number of latent dimensions.
     """
 
-    def __init__(self, sess, num_classes,
+    def __init__(self, sess, num_classes, learn_phase=None, log_every=50,
                  only_cpu=False, logdir='model/', num_latent=100):
         self._num_latent = num_latent
         self._logdir = logdir
@@ -48,6 +80,8 @@ class SeqGAN(object):
         self._num_classes = num_classes
         self._only_cpu = only_cpu
         self._weights = []
+        self._learn_phase = learn_phase
+        self.log_every = log_every
 
         # Builds the various placeholders.
         self._text_len_pl = tf.placeholder(
@@ -85,6 +119,11 @@ class SeqGAN(object):
     @property
     def logdir(self):
         return self._logdir
+
+    @check_built
+    @property
+    def current_time(self):
+        return self._sess.run(self.time)
 
     def _generate_latent_variable(self, batch_size):
         return np.random.normal(size=(batch_size, self.num_latent))
@@ -142,18 +181,19 @@ class SeqGAN(object):
 
         return weight
 
-    def build_generator(self, num_rnns=3, rnn_dims=128, scope='generator'):
+    def build_generator(self, use_multinomial=True, num_rnns=3, rnn_dims=128):
         """Builds the generator part of the model.
         Args:
-            num_rnns: int, number of RNNs to stack.
-            rnn_dims: int, number of outputs of the RNN.
-            scope: str (default: "generator"), the scope of this part
-                of the model.
+            use_multinomial: bool (default: True), whether or not to sample
+                from a multinomial distribution for each consecutive step of
+                the RNN.
+            num_rnns: int (default: 128), number of RNNs to stack.
+            rnn_dims: int (default: 3), number of outputs of the RNN.
         Returns:
             a tensor representing the generated question.
         """
 
-        with tf.variable_scope(scope):
+        with tf.variable_scope('generator'):
 
             # Creates the RNN output -> model output function.
             output_W = self.get_weight('output_W', (rnn_dims, self.num_classes))
@@ -161,6 +201,7 @@ class SeqGAN(object):
 
             # Creates the RNN cell.
             cells = [tf.contrib.rnn.GRUCell(rnn_dims) for _ in range(num_rnns)]
+            cells = [tf.contrib.rnn.DropoutWrapper(cell, 0.7) for cell in cells]
             cell = tf.contrib.rnn.MultiRNNCell(cells)
 
             def _add_proj(i, activation=tf.tanh):
@@ -173,47 +214,88 @@ class SeqGAN(object):
             # Creates the initial encoder state by mapping from latent dim.
             encoder_state = tuple(_add_proj(i) for i in range(num_rnns))
 
-            # Creates the embeddings (just one-hot encodings).
-            embeddings = tf.eye(self.num_classes)
+            # Gets the batch size from the latent pl.
+            batch_size = tf.shape(self.latent_pl)[0]
 
-            # Builds the inferrence functions.
-            infer_fn = tf.contrib.seq2seq.simple_decoder_fn_inference(
-                output_fn=output_fn,
-                encoder_state=encoder_state,
-                embeddings=embeddings,
-                start_of_sequence_id=utils.START_IDX,
-                end_of_sequence_id=-1,
-                maximum_length=self.text_len_pl - 1,
-                num_decoder_symbols=self.num_classes,
-                name='decoder_inference_fn')
+            # Creates the teacher forcing op.
+            teacher_inp = tf.concat([tf.zeros_like(self.text_pl[:, :1]),
+                                     self.text_pl[:, :-1]], axis=1)
+            teacher_inp = tf.one_hot(teacher_inp, self.num_classes)
+            teacher_fn = tf.contrib.seq2seq.simple_decoder_fn_train(
+                encoder_state)
+            seq_len = tf.ones((batch_size,), 'int32') * self.text_len_pl
+            teacher_preds, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(
+                cell=cell,
+                inputs=teacher_inp,
+                decoder_fn=teacher_fn,
+                sequence_length=seq_len)
+            teacher_preds = tf.einsum('ijk,kl->ijl', teacher_preds, output_W)
+            teach_loss = tf.contrib.seq2seq.sequence_loss(
+                logits=teacher_preds,
+                targets=self.text_pl,
+                weights=tf.ones((batch_size, self.text_len_pl)))
+            teach_loss = tf.reduce_mean(teach_loss)
+
+            # Reuses generator variables for the inference part.
+            tf.get_variable_scope().reuse_variables()
+
+            if use_multinomial:
+                def infer_fn(time, state, input_var, output_var, ctx):
+                    if output_var is None:
+                        next_id = tf.zeros((batch_size,), 'int32')
+                        state = encoder_state
+                        output_var = tf.zeros((self.num_classes,))
+
+                    else:
+                        output_var = output_fn(output_var)
+                        next_id = multinomial_2d(output_var)
+
+                    next_input = tf.one_hot(next_id, self.num_classes)
+                    done = tf.cond(tf.greater_equal(time, self.text_len_pl),
+                                   lambda: tf.ones((batch_size,), 'bool'),
+                                   lambda: tf.zeros((batch_size,), 'bool'))
+
+                    return done, state, next_input, output_var, ctx
+
+            else:
+                embeddings = tf.eye(self.num_classes)
+                infer_fn = tf.contrib.seq2seq.simple_decoder_fn_inference(
+                    output_fn=output_fn,
+                    encoder_state=encoder_state,
+                    embeddings=embeddings,
+                    start_of_sequence_id=0,
+                    end_of_sequence_id=-1,
+                    maximum_length=self.text_len_pl - 1,
+                    num_decoder_symbols=self.num_classes,
+                    name='decoder_inference_fn')
 
             generated_sequence, _, _ = tf.contrib.seq2seq.dynamic_rnn_decoder(
                 cell=cell,
                 decoder_fn=infer_fn)
 
-            generatd_sequence = tf.nn.softmax(generated_sequence)
+            class_scores = tf.nn.softmax(generated_sequence)
+            generated_sequence = tf.argmax(generated_sequence, axis=-1)
+            # generated_sequence = multinomial_3d(generated_sequence)
 
-            generator_weights = get_scope_variables(scope)
+        tf.summary.scalar('loss/teacher', teach_loss)
 
-        return generated_sequence, generator_weights
+        return class_scores, generated_sequence, teach_loss
 
     def build_discriminator(self, input_tensor, reuse=False, num_rnns=3,
-                            rnn_dims=128, scope='discriminator'):
+                            rnn_dims=128):
         """Builds the discriminator part of the model.
         Args:
             input_tensor: Tensor with shape (batch_size, num_timesteps), where
                 each value is an integer token index.
             reuse: bool (default: False), if set, reuse variable weights.
             num_rnns: int, number of RNNs to stack.
-            rnn_dims: int, number of outputs of the RNN.
-            scope: str (default: "discriminator"), the scope of this part
                 of the model.
         Returns:
             a tensor with shape (batch_size) that predicts whether the input
                 tensor is real or fake.
         """
 
-        with tf.variable_scope(scope):
+        with tf.variable_scope('discriminator'):
 
             if reuse:
                 tf.get_variable_scope().reuse_variables()
@@ -234,48 +316,150 @@ class SeqGAN(object):
             # Reduces to binary prediction.
             pred_W = self.get_weight('pred_W', (rnn_dims, 1))
             preds = tf.einsum('ijk,kl->ijl', rnn_output, pred_W)
-            preds = tf.squeeze(tf.sigmoid(preds), axis=-1)
+            preds = tf.sigmoid(preds)
 
-            discriminator_weights = get_scope_variables(scope)
+        return preds
 
-        return preds, discriminator_weights
+    def get_discriminator_op(self, r_preds, g_preds, d_weights):
+        """Returns an op that updates the discriminator weights correctly.
+        Args:
+            r_preds: Tensor with shape (batch_size, num_timesteps, 1), the
+                discriminator predictions for real data.
+            g_preds: Tensor with shape (batch_size, num_timesteps, 1), the
+                discriminator predictions for generated data.
+            d_weights: a list of trainable tensors representing the weights
+                associated with the discriminator model.
+        Returns:
+            dis_op, the op to run to train the discriminator.
+        """
 
-    def build(self):
-        """Builds the model."""
+        with tf.variable_scope('loss/discriminator'):
 
-        g_sequence, g_weights = self.build_generator()
-        g_argmax = tf.argmax(g_sequence, axis=-1)
-        r_preds, d_weights = self.build_discriminator(self.text_pl)
-        g_preds, _ = self.build_discriminator(g_argmax, reuse=True)
+            # Creates the optimizer.
+            discriminator_opt = tf.train.AdamOptimizer(1e-4)
+
+            # Computes log loss on real and generated sequences.
+            eps = 1e-12
+            r_loss = -tf.reduce_mean(tf.log(r_preds + eps))  # r_preds -> 1.
+            f_loss = -tf.reduce_mean(tf.log(1 - g_preds + eps))  # g_preds -> 0.
+            dis_loss = r_loss + f_loss
+
+            # Adds summaries.
+            tf.summary.scalar('real', r_loss)
+            tf.summary.scalar('generated', f_loss)
+
+            # Adds discriminator regularization loss.
+            with tf.variable_scope('regularization'):
+                dis_reg_loss = sum([tf.nn.l2_loss(w) for w in d_weights]) * 1e-4
+            tf.summary.scalar('regularization', dis_reg_loss)
+
+            # Minimizes the discriminator loss
+            total_loss = dis_loss + dis_reg_loss
+            dis_op = discriminator_opt.minimize(total_loss, var_list=d_weights)
+            tf.summary.scalar('total', total_loss)
+
+        return dis_op
+
+    def get_generator_op(self, g_sequence, d_preds, g_preds, g_weights):
+        """Returns an op that updates the generator weights correctly.
+        Args:
+            g_sequence: Tensor with shape (batch_size, num_timesteps) where
+                each value is the token predicted by the generator.
+            d_preds: Tensor with shape (batch_size, num_timesteps, 1)
+                representing the output of the discriminator on the generated
+                sequence.
+            g_preds: Tensor with shape (batch_size, num_timesteps, num_classes)
+                representing the softmax distribution over generator classes.
+            g_weights: a list of trainable tensors representing the weights
+                associated with the generator model.
+        Returns:
+            gen_op, the op to run to train the generator.
+        """
+
+        with tf.variable_scope('loss/generator'):
+
+            # Creates the optimizer.
+            generator_opt = tf.train.AdamOptimizer(1e-4)
+
+            # This variable represents the mask of what the model ended up
+            # choosing. We want to ignore any of the output values that it
+            # didn't choose, and either increase or decrease the probability
+            # of the states in `g_sequence` depending on whether or not the
+            # reward was good or bad.
+            g_sequence = tf.one_hot(g_sequence, self.num_classes)
+
+            # This is the actual tensor representing the class predictions.
+            # We'll apply `g_sequence` as a mask to ignore the non-important
+            # parts.
+            g_preds = tf.clip_by_value(g_preds, 1e-20, 1.)
+
+            # The actual "reward" is the output of the discriminator. If the
+            # discriminator liked the values it saw, increase the probability of
+            # those classes.
+            gen_reward = g_sequence * g_preds * d_preds
+            gen_loss = -tf.reduce_mean(gen_reward)
+            tf.summary.scalar('reward', gen_loss)
+
+            # Adds generator regularization loss.
+            with tf.variable_scope('regularization'):
+                gen_reg_loss = sum([tf.nn.l2_loss(w) for w in g_weights]) * 1e-5
+            tf.summary.scalar('regularization', gen_reg_loss)
+
+            # Minimizes the generator loss.
+            total_loss = gen_loss + gen_reg_loss
+            gen_op = generator_opt.minimize(total_loss, var_list=g_weights)
+            tf.summary.scalar('total', total_loss)
+
+        return gen_op
+
+    def build(self, reg_loss=1e-4, use_teacher=True):
+        """Builds the model.
+        Args:
+            reg_loss: float, how much to weight regularization loss.
+            use_teacher: bool, if set, use the teacher.
+        """
+
+        g_classes, g_seq, teach_loss = self.build_generator()
+        r_preds = self.build_discriminator(self.text_pl)
+        g_preds = self.build_discriminator(g_seq, reuse=True)
+
+        g_weights = get_scope_variables('generator')
+        d_weights = get_scope_variables('discriminator')
+
+        # Adds summaries of the real and fake predictions.
+        tf.summary.histogram('predictions/fake', g_preds)
+        tf.summary.histogram('predictions/real', r_preds)
 
         # Captures the generated sequence to use later.
-        self.generated_sequence = g_argmax
+        self.generated_sequence = g_seq
 
-        # Creates the discriminator loss function.
-        eps = 1e-12
-        dis_loss = -tf.log(r_preds + eps) - tf.log(1 - g_preds)
+        # Computes the weight updates for the discriminator and generator.
+        dis_op = self.get_discriminator_op(r_preds, g_preds, d_weights)
+        gen_op = self.get_generator_op(g_seq, g_preds, g_classes, g_weights)
 
-        # Creates separate generator and discriminator optimizers.
-        generator_opt = tf.train.AdamOptimizer(learning_rate=1e-4)
-        discriminator_opt = tf.train.AdamOptimizer(learning_rate=3e-4)
-
-        # Creates the discriminator minimization op.
-        dis_op = discriminator_opt.minimize(dis_loss)
-
-        # Creates the generator policy.
-        policy = tf.expand_dims(1 - 2 * g_preds, axis=-1)
-        ohe = tf.one_hot(g_argmax, self.num_classes)
-        generator_grad_loss = policy * ohe
-
-        # Creates the generator minimization op.
-        gvs = generator_opt.compute_gradients(
-            g_sequence, g_weights, grad_loss=generator_grad_loss)
-        gen_op = generator_opt.apply_gradients(gvs)
+        # Adds the teacher forcing part, decaying at some rate.
+        if use_teacher:
+            teach_lr = 10000. / (10000. + tf.cast(self.time, 'float32'))
+            teach_lr *= 1e-3
+            teach_opt = tf.train.AdamOptimizer(teach_lr)
+            teach_op = teach_opt.minimize(teach_loss)
+            gen_op = tf.group(gen_op, teach_op)
+            tf.summary.scalar('teacher_lr', teach_lr)
 
         # Creates op to update time.
         step_op = self.time.assign(self.time + 1)
 
-        self.train_op = tf.group(gen_op, dis_op, step_op)
+        # Allows the user to specify sequential vs. simultaneous updates.
+        if self._learn_phase is None:
+            gan_train_op = tf.group(gen_op, dis_op)
+        else:
+            gan_train_op = tf.cond(
+                tf.equal(tf.mod(self.time, self._learn_phase), 0),
+                lambda: gen_op,
+                lambda: dis_op)
+
+        # Updates time every step.
+        self.train_op = tf.group(gan_train_op, step_op)
 
         # Creates the log directory and saving objects.
         if self.logdir is None:
@@ -332,7 +516,13 @@ class SeqGAN(object):
             self.latent_pl: latent,
         }
 
-        _ = self._sess.run([self.train_op], feed_dict=feed_dict)
+        t = self._sess.run(self.time)
+        if t % self.log_every:
+            self._sess.run(self.train_op, feed_dict=feed_dict)
+        else:
+            _, summary = self._sess.run([self.train_op, self.summary_op],
+                                        feed_dict=feed_dict)
+            self.summary_writer.add_summary(summary, t)
 
     @check_built
     def generate(self, sample_len):
--- train.py
+++ train.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
-"""Trains the model on some data, and saves it locally."""
+"""Trains the model on some data, and saves it locally.
+
+The default training text is the `lorem.txt` file, which consists of some
+lorem ipsum text. The character frequencies in this text are as follows:
+
+';' -> 2		'L' -> 8		'O' -> 12		'E' -> 34
+'F' -> 39		'Q' -> 39		'U' -> 46		'I' -> 68
+'j' -> 71		'A' -> 75		'V' -> 77		'C' -> 95
+'S' -> 100		'x' -> 103		'D' -> 106		'M' -> 116
+'P' -> 149		'N' -> 178		'\n' -> 200		'h' -> 271
+'f' -> 431		'q' -> 593		'g' -> 620		'b' -> 625
+'v' -> 771		',' -> 861		'p' -> 1085		'.' -> 1138
+'d' -> 1416		'c' -> 2050		'o' -> 2184		'm' -> 2319
+'r' -> 2833		'n' -> 2916		'l' -> 3017		'a' -> 3969
+'t' -> 4194		's' -> 4246		'u' -> 4514		'i' -> 5111
+'e' -> 5643		' ' -> 8998
+"""
 
 from __future__ import print_function
 
@@ -16,7 +32,7 @@ if __name__ == '__main__':
         description='Train a SeqGAN model on some text.')
     parser.add_argument('-t', '--text', default='lorem.txt', type=str,
                         help='path to the text to use')
-    parser.add_argument('-l', '--seq_len', default=50, type=int,
+    parser.add_argument('-l', '--seq_len', default=100, type=int,
                         help='the length of each training sequence')
     parser.add_argument('-b', '--batch_size', default=32, type=int,
                         help='size of each training batch')
@@ -26,6 +42,8 @@ if __name__ == '__main__':
                         help='number of training epochs')
     parser.add_argument('-c', '--only_cpu', default=False, action='store_true',
                         help='if set, only build weights on cpu')
+    parser.add_argument('-p', '--learn_phase', default=None, type=int,
+                        help='learning phase (None for synchronized)')
 
     args = parser.parse_args()
 
@@ -43,17 +61,21 @@ if __name__ == '__main__':
                               seq_len=args.seq_len)
 
     sess = tf.Session()
-    model = SeqGAN(sess, num_classes, only_cpu=args.only_cpu)
+    model = SeqGAN(sess,
+                   num_classes,
+                   learn_phase=args.learn_phase,
+                   only_cpu=args.only_cpu)
     model.build()
+    model.load(ignore_missing=True)
 
-    for epoch in xrange(args.num_epochs):
+    for epoch in xrange(1, args.num_epochs + 1):
         for step in xrange(args.num_steps):
             logging.info('epoch %d, step %d', epoch, step)
             model.train_batch(iterator.next())
 
         # Generates a sample from the model.
-        g = model.generate(100)
-        logging.info('Epoch %d: "%s"', epoch, utils.detokenize(g, rev_dict))
+        g = model.generate(args.seq_len)
+        logging.info('epoch %d: "%s"', epoch, utils.detokenize(g, rev_dict))
 
         # Saves the model to the logdir.
         model.save()
